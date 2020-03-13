a0cad69fc679cdb48a659035b9361540a9170791
phatlt <phatlt@eplatform.vn>
2020-03-13 08:17:27 +0700

    add addProduct

---
+++ src/components/page/addProduct.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="py-5 bg-light">
+    add product
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddProduct",
+  data() {
+    return {
+    }
+  },
+
+  created() {
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
