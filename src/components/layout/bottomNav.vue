3e9ade30c65592f29c2dfb05cea3ebc351981c23
PhatLT1 <phatlt2908@gmail.com>
2020-07-26 12:57:02 +0700

    update layout home

--- src/components/layout/bottomNav.vue
+++ src/components/layout/bottomNav.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="d-block d-sm-none">
-    <div class="fixed-bottom row no-gutters text-center shadow-lg bg-white">
-      <a href="" class="col-3">1</a>
-      <a href="" class="col-3">2</a>
-      <a href="" class="col-3">3</a>
-      <a href="" class="col-3">4</a>
+    <div class="fixed-bottom row no-gutters text-center shadow-lg bg-white py-1">
+      <a href="" class="col-3 active">
+        <font-awesome-icon class="bottom-icon" icon="bell" />
+        <div class="bottom-text">Trang chủ</div>
+      </a>
+      <a href="" class="col-3">
+        <font-awesome-icon class="bottom-icon" icon="bell" />
+        <div class="bottom-text">Trang chủ</div>
+      </a>
+      <a href="" class="col-3">
+        <font-awesome-icon class="bottom-icon" icon="bell" />
+        <div class="bottom-text">Trang chủ</div>
+      </a>
+      <a href="" class="col-3">
+        <font-awesome-icon class="bottom-icon" icon="bell" />
+        <div class="bottom-text">Trang chủ</div>
+      </a>
     </div>
   </div>
 </template>
@@ -20,4 +32,15 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.bottom-icon {
+  color: #f05f95;
+  font-size: 20px;
+}
+a .bottom-text {
+  font-size: 10px;
+  font-weight: 100;
+  line-height: 1.5;
+  color: #000;
+}
+</style>
